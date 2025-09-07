9dc5e3ac81f88e4de298379ffdcf3661cbceddf1
Ahmed Asar <a7med3sar22@gmail.com>
2025-09-07 16:13:17 +0300

    Consuming the GET and POST API from Javascript/ HTML

---
+++ Consuming-Python-Api-from-Javascript/index.py
@@ -0,0 +1,45 @@
+import tornado.web
+import tornado.ioloop
+import json
+
+
+class MainRequestHandler(tornado.web.RequestHandler):
+    """Handles the root URL ('/') and sends a simple text response."""
+
+    def get(self):
+        self.render("index.html")
+
+
+class FruitListApiHandler(tornado.web.RequestHandler):
+    """
+    Reads a list of fruits from 'list.txt' and returns them as a array.
+    This version includes error handling for the missing file.
+    """
+
+    def get(self):
+        fh = open("list.txt", "r")
+        fruits = fh.read().splitlines()
+        fh.close()
+        self.write(json.dumps(fruits))
+
+    def post(self):
+        fh = open("list.txt", "a")
+        fruit = self.get_argument("fruit", None)
+        fh.write(f"{fruit}\n")
+        fh.close()
+        self.write(json.dumps({"message": "Fruit added successfully"}))
+
+
+if __name__ == "__main__":
+    # The application routes map URLs to their respective handler classes.
+    app = tornado.web.Application(
+        [
+            (r"/", MainRequestHandler),
+            (r"/list", FruitListApiHandler),
+        ],
+    )
+
+    port = 8882
+    app.listen(port)
+    print(f"Application is ready and listening on http://localhost:{port}")
+    tornado.ioloop.IOLoop.current().start()
